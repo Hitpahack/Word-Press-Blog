cdfbd74764112c5b5b5ed4eb53b441fe20000bd6
Hitesh <hiteshpatodia1@gmail.com>
2025-03-07 18:50:07 +0530

    initial commit

---
+++ Repository/ISingleFirstOrDefault.cs
@@ -0,0 +1,185 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
+
+namespace WP.Repository
+{
+    public interface ISingleFirstOrDefault<TEntity>
+    {
+        /// <summary>
+        /// Gets the first or default entity based on a predicate, orderby delegate and include delegate. This method defaults to a read-only, no-tracking query.
+        /// </summary>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <returns>An <see cref="{TEntity}"/> that contains elements that satisfy the condition specified by <paramref name="predicate"/>.</returns>
+        /// <remarks>This method defaults to a read-only, no-tracking query.</remarks>
+        TEntity GetFirstOrDefault(Expression<Func<TEntity, bool>> predicate);
+        /// <summary>
+        /// Gets the first or default entity based on a predicate, orderby delegate and include delegate. This method defaults to a read-only, no-tracking query.
+        /// </summary>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <param name="include">A function to include navigation properties</param>
+        /// <returns>An <see cref="{TEntity}"/> that contains elements that satisfy the condition specified by <paramref name="predicate"/>.</returns>
+        /// <remarks>This method defaults to a read-only, no-tracking query.</remarks>
+        TEntity GetFirstOrDefault(Expression<Func<TEntity, bool>> predicate, Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include);
+        TEntity GetFirstOrDefault(Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include, Expression<Func<TEntity, bool>> predicate);
+        /// <summary>
+        /// Gets the first or default entity based on a predicate, orderby delegate and include delegate. This method defaults to a read-only, no-tracking query.
+        /// </summary>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <param name="orderBy">A function to order elements.</param>
+        /// <param name="include">A function to include navigation properties</param>
+        /// <returns>An <see cref="{TEntity}"/> that contains elements that satisfy the condition specified by <paramref name="predicate"/>.</returns>
+        /// <remarks>This method defaults to a read-only, no-tracking query.</remarks>
+        TEntity GetFirstOrDefault(Expression<Func<TEntity, bool>> predicate,
+                                  Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include,
+                                  Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy);
+
+
+        /// <summary>
+        /// Gets the first or default entity based on a predicate, orderby delegate and include delegate. This method defaults to a read-only, no-tracking query.
+        /// </summary>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <returns>An <see cref="{TEntity}"/> that contains elements that satisfy the condition specified by <paramref name="predicate"/>.</returns>
+        /// <remarks>This method defaults to a read-only, no-tracking query.</remarks>
+        TResult GetFirstOrDefaultMap<TResult>(Expression<Func<TEntity, bool>> predicate );
+
+        /// <summary>
+        /// Gets the first or default entity based on a predicate, orderby delegate and include delegate. This method defaults to a read-only, no-tracking query.
+        /// </summary>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <param name="include">A function to include navigation properties</param>
+        /// <returns>An <see cref="{TEntity}"/> that contains elements that satisfy the condition specified by <paramref name="predicate"/>.</returns>
+        /// <remarks>This method defaults to a read-only, no-tracking query.</remarks>
+        TResult GetFirstOrDefaultMap<TResult>(Expression<Func<TEntity, bool>> predicate,
+                                  Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include);
+
+
+        /// <summary>
+        /// Gets the first or default entity based on a predicate, orderby delegate and include delegate. This method defaults to a read-only, no-tracking query.
+        /// </summary>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <param name="orderBy">A function to order elements.</param>
+        /// <param name="include">A function to include navigation properties</param>
+        /// <returns>An <see cref="{TEntity}"/> that contains elements that satisfy the condition specified by <paramref name="predicate"/>.</returns>
+        /// <remarks>This method defaults to a read-only, no-tracking query.</remarks>
+        TResult GetFirstOrDefaultMap<TResult>(Expression<Func<TEntity, bool>> predicate,
+                                  Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include,
+                                  Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy);
+
+
+
+        /// <summary>
+        /// Gets the first or default entity based on a predicate, orderby delegate and include delegate. This method defaults to a read-only, no-tracking query.
+        /// </summary>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <returns>An <see cref="{TEntity}"/> that contains elements that satisfy the condition specified by <paramref name="predicate"/>.</returns>
+        /// <remarks>This method defaults to a read-only, no-tracking query.</remarks>
+        TEntity GetSingleOrDefault(Expression<Func<TEntity, bool>> predicate);
+
+        /// <summary>
+        /// Gets the first or default entity based on a predicate, orderby delegate and include delegate. This method defaults to a read-only, no-tracking query.
+        /// </summary>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <param name="include">A function to include navigation properties</param>
+        /// <returns>An <see cref="{TEntity}"/> that contains elements that satisfy the condition specified by <paramref name="predicate"/>.</returns>
+        /// <remarks>This method defaults to a read-only, no-tracking query.</remarks>
+        TEntity GetSingleOrDefault(Expression<Func<TEntity, bool>> predicate,
+                                  Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include);
+
+
+        /// <summary>
+        /// Gets the first or default entity based on a predicate, orderby delegate and include delegate. This method defaults to a read-only, no-tracking query.
+        /// </summary>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <param name="orderBy">A function to order elements.</param>
+        /// <param name="include">A function to include navigation properties</param>
+        /// <returns>An <see cref="{TEntity}"/> that contains elements that satisfy the condition specified by <paramref name="predicate"/>.</returns>
+        /// <remarks>This method defaults to a read-only, no-tracking query.</remarks>
+        TEntity GetSingleOrDefault(Expression<Func<TEntity, bool>> predicate,
+                                  Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include,
+                                  Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy);
+
+
+        /// <summary>
+        /// Gets the first or default entity based on a predicate, orderby delegate and include delegate. This method defaults to a read-only, no-tracking query.
+        /// </summary>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <param name="orderBy">A function to order elements.</param>
+        /// <param name="include">A function to include navigation properties</param>
+        /// <param name="disableTracking"><c>true</c> to disable changing tracking; otherwise, <c>false</c>. Default to <c>true</c>.</param>
+        /// <param name="ignoreQueryFilters">Ignore query filters</param>
+        /// <returns>An <see cref="{TEntity}"/> that contains elements that satisfy the condition specified by <paramref name="predicate"/>.</returns>
+        /// <remarks>This method defaults to a read-only, no-tracking query.</remarks>
+        TEntity GetSingleOrDefault(Expression<Func<TEntity, bool>> predicate = null,
+                                  Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
+                                  Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+                                   bool disableTracking = true,
+                                           bool ignoreQueryFilters = false);
+
+        /// <summary>
+        /// Gets the first or default entity based on a predicate, orderby delegate and include delegate. This method defaults to a read-only, no-tracking query.
+        /// </summary>
+        /// <param name="selector">The selector for projection.</param>
+        /// <returns>An <see cref="{TEntity}"/> that contains elements that satisfy the condition specified by <paramref name="predicate"/>.</returns>
+        /// <remarks>This method defaults to a read-only, no-tracking query.</remarks>
+        TResult GetFirstOrDefault<TResult>(Expression<Func<TEntity, TResult>> selector);
+
+        /// <summary>
+        /// Gets the first or default entity based on a predicate, orderby delegate and include delegate. This method defaults to a read-only, no-tracking query.
+        /// </summary>
+        /// <param name="selector">The selector for projection.</param>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <returns>An <see cref="{TEntity}"/> that contains elements that satisfy the condition specified by <paramref name="predicate"/>.</returns>
+        /// <remarks>This method defaults to a read-only, no-tracking query.</remarks>
+        TResult GetFirstOrDefault<TResult>(Expression<Func<TEntity, TResult>> selector,
+                                           Expression<Func<TEntity, bool>> predicate);
+
+
+        /// <summary>
+        /// Gets the first or default entity based on a predicate, orderby delegate and include delegate. This method defaults to a read-only, no-tracking query.
+        /// </summary>
+        /// <param name="selector">The selector for projection.</param>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <param name="include">A function to include navigation properties</param>
+        /// <returns>An <see cref="{TEntity}"/> that contains elements that satisfy the condition specified by <paramref name="predicate"/>.</returns>
+        /// <remarks>This method defaults to a read-only, no-tracking query.</remarks>
+        TResult GetFirstOrDefault<TResult>(Expression<Func<TEntity, TResult>> selector,
+                                           Expression<Func<TEntity, bool>> predicate,
+                                           Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include);
+
+        
+
+        /// <summary>
+        /// Gets the first or default entity based on a predicate, orderby delegate and include delegate. This method defaults to a read-only, no-tracking query.
+        /// </summary>
+        /// <param name="selector">The selector for projection.</param>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <param name="orderBy">A function to order elements.</param>
+        /// <param name="include">A function to include navigation properties</param>
+        /// <returns>An <see cref="{TEntity}"/> that contains elements that satisfy the condition specified by <paramref name="predicate"/>.</returns>
+        /// <remarks>Ex: This method defaults to a read-only, no-tracking query.</remarks>
+        Task<TResult> GetFirstOrDefaultAsync<TResult>(Expression<Func<TEntity, TResult>> selector,
+          Expression<Func<TEntity, bool>> predicate,
+          Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include,
+          Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy);
+
+        /// <summary>
+        /// Gets the first or default entity based on a predicate, orderby delegate and include delegate. This method defaults to a read-only, no-tracking query.
+        /// </summary>
+        /// <param name="selector">The selector for projection.</param>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <param name="orderBy">A function to order elements.</param>
+        /// <param name="include">A function to include navigation properties</param>
+        /// <param name="disableTracking"><c>true</c> to disable changing tracking; otherwise, <c>false</c>. Default to <c>true</c>.</param>
+        /// <param name="ignoreQueryFilters">Ignore query filters</param>
+        /// <returns>An <see cref="{TEntity}"/> that contains elements that satisfy the condition specified by <paramref name="predicate"/>.</returns>
+        /// <remarks>Ex: This method defaults to a read-only, no-tracking query.</remarks>
+        Task<TResult> GetFirstOrDefaultAsync<TResult>(Expression<Func<TEntity, TResult>> selector, 
+            Expression<Func<TEntity, bool>> predicate,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include, 
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy, 
+            bool disableTracking = true, bool ignoreQueryFilters = false);
+
+
+       
+    }
+}
